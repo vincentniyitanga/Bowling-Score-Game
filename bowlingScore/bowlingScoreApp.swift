f21200bb442a0a5aeee75d5b308dd46eaa7cbff1
vincentniyitanga <vincentniyitanga@mail.weber.edu>
2024-06-02 18:15:07 -0600

    Initial Commit

---
+++ bowlingScore/bowlingScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  bowlingScoreApp.swift
+//  bowlingScore
+//
+//  Created by Vincent Niyitanga on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct bowlingScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
